5c2555c618a6b4d341db1b535e549ae6ca4a1463
Medini Chopra <medini.chopra_ug23@ashoka.edu.in>
2022-11-18 15:27:55 +0530

    add client and server

---
+++ client.py
@@ -0,0 +1,85 @@
+import socket
+import json
+
+
+host = socket.gethostname()  # as both code is running on same pc
+port = 8728  # socket server port number
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # instantiate, 
+client_socket.connect((host, port))  # connect to the server
+
+# This is where the sequence will begin, client waits for intro message
+print(client_socket.recv(3000).decode()) 
+client_socket.send('OK'.encode())  # Send OK message
+
+# This is where the registration and authentication will happen
+print(client_socket.recv(3000).decode()) # Asks if user is registered
+user = input() #takes Y/N from user accordingly
+client_socket.send(user.encode()) #sends Y/N to server
+print()
+
+if user == 'N':
+    print(client_socket.recv(3000).decode()) #asks for email
+    email = input() #takes email from user
+    client_socket.send(email.encode()) #sends email to server
+    print()
+    if email.endswith('@ashoka.edu.in'):
+        print(client_socket.recv(3000).decode()) #asks for password
+        password = input()
+        client_socket.send(password.encode()) #sends password to server
+        print()
+        print(client_socket.recv(3000).decode()) #logged in message
+        client_socket.send('OK'.encode())  # Send OK message
+    else:
+        print(client_socket.recv(3000).decode()) #prints error mesage
+elif user == 'Y':
+    print(client_socket.recv(3000).decode()) #asks for email
+    email = input() #takes email from user
+    client_socket.send(email.encode()) #sends email to server
+    print()
+    if email.endswith('@ashoka.edu.in'):
+        print(client_socket.recv(3000).decode()) #asks for password
+        password = input()
+        client_socket.send(password.encode()) #sends password to server
+        print()
+        print(client_socket.recv(3000).decode()) #logged in message
+        client_socket.send('OK'.encode())  # Send OK message
+    else:
+        print(client_socket.recv(3000).decode()) #prints error mesage
+else: 
+    #wait for error message
+    print()
+    print(client_socket.recv(3000).decode())
+
+# This is where the client will receive the service options
+options = client_socket.recv(3000).decode() 
+options = json.loads(options) # Gets options Prereqs and Semester
+
+print("We offer the following services: ")
+for option in options:
+    print(option) # check if this is correct
+
+print('\nWhat would you like to view? (Prereqs/Sem): ', end='')  # show in terminal
+choice = input()
+client_socket.send(choice.encode())
+print()
+
+if choice == 'Prereqs':
+    #wait for outro
+    var = input(client_socket.recv(3000).decode())
+    client_socket.send(var.encode())
+    print() #print ok
+    print(client_socket.recv(3000).decode())
+elif choice == 'Sem':
+    #wait for service request
+    var = input(client_socket.recv(3000).decode())
+    #send service needed 
+    client_socket.send(var.encode())
+    #wait for ok response
+    print() 
+    print(client_socket.recv(3000).decode())
+else: 
+    #wait for error message
+    print()
+    print(client_socket.recv(3000).decode())
+
+client_socket.close()  # close the connection
---
+++ server.py
@@ -0,0 +1,130 @@
+import socket
+import json 
+import random
+
+hostIP = '0.0.0.0'
+port=8728
+ServerSocket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ServerSocket.bind((hostIP,port)) #binds the socket to the port
+ServerSocket.listen(3)
+print("Server is up!")
+
+options = ["Prerequisites for courses", "Semester-wise courses"]
+pre_req = {"CS-1101" : "None", "CS-1104" : "None", "CS-1209" : "None", "CS-1216" : "Introduction to Computer Programming", "CS-1203" : "Introduction to Computer Programming", "CS-1205" : "Introduction to Computer Programming", "CS-1217" : "Introduction to Computer Programming and Computer Organization and Systems", "CS-1390" : "Introduction to Computer Programming and Probability and Statistics", "CS-1340" : "Introduction to Computer Programming and Computer Organization and Systems", "CS-1319" : "Introduction to Computer Programming, Computer Organization and Systems, and Data Structures"}
+sem = {"Monsoon 2022" : "CS-1101 Introduction to Computer Programming, Computer Organisation and Systems, Data Structures, Probability and Statistics, Introduction to Machine Learning, Computer Networks, Programming Language Design and Implementation",
+"Spring 2022" : "CS-1101 Introduction to Computer Programming, CS-1104 Discrete Mathematics, Operating Systems, Algorithm Design and Analysis"}
+
+def handle_new_client(ClientSocket,Address):
+    global options
+    global pre_req
+    global sem
+
+    # Response is ok, so sends service options
+    data = json.dumps(options)
+    ClientSocket.send(data.encode()) #send options i.e. Prereqs or Sem
+
+    #send available information
+    choice = ClientSocket.recv(3000).decode() #Prereq or Sem
+    if choice == 'Prereqs':
+        ClientSocket.send("Send us the course for which you'd like prerequisites! (CS-1101/CS-1104/CS-1209/CS-1216/CS-1203/CS-1205/CS-1217/CS-1390/CS-1340/CS-1319):\n".encode()) 
+        #wait for response
+        code = ClientSocket.recv(3000).decode()
+        if(code in pre_req.keys()):
+            data_sent = pre_req[code]
+            ClientSocket.send(str(data_sent).encode())
+    elif choice == "Sem":
+        ClientSocket.send("Send us the semester for which you'd like courses! (Monsoon 2022/Spring 2022):\n".encode()) 
+        #wait for response
+        sems = ClientSocket.recv(3000).decode()
+        if(sems in sem.keys()):
+            data_sent = sem[sems]
+        ClientSocket.send(str(data_sent).encode())
+    else:
+        ClientSocket.send("Invalid choice! Disconnecting...".encode())
+
+while True:
+    (ClientSocket,Address) = ServerSocket.accept() #wait until client connectss
+
+    print("New client connected!")
+    intro = "\nWelcome to the Course Catalogue!\n"
+    ClientSocket.send(intro.encode())
+    # Sends intro message
+
+    # Wait for OK response
+    # If response not ok thesn
+    if(ClientSocket.recv(3000).decode() != "OK"):
+        print("Disconnecting! Error!!")
+        ClientSocket.close()
+
+    # OPEN USERS FILE HERE
+    with open("users.json", "r") as jsonFile:
+        users = json.load(jsonFile)
+    #this is where we add registration and authentication with ashoka ID
+    ClientSocket.send("Hello! Are you a registered user? (Y/N):".encode()) #Ask if user is registered
+    user = ClientSocket.recv(3000).decode()
+    if(user == "N"):
+        ClientSocket.send("Enter your Ashoka email:".encode())
+        #wait for response
+        email = ClientSocket.recv(3000).decode() #gets email from client
+        if email.endswith('@ashoka.edu.in'):
+            num = "10" #str(random.randint(10))
+            ClientSocket.send(f"PWD: {num} A password has been sent to your email. Enter your password:".encode())
+            #generate and send password to email
+            password = ClientSocket.recv(3000).decode()
+            
+            #check if email actually exists
+            if password == num:
+                print("in here")
+                users[email] = password
+                ClientSocket.send("You have been logged in successfully.".encode())
+                if(ClientSocket.recv(3000).decode() != "OK"):
+                    print("Disconnecting! Error!!")
+                    ClientSocket.close()
+                handle_new_client(ClientSocket,Address)
+        else:
+            ClientSocket.send("Invalid email".encode())
+            #do equivalent of return 0
+
+    elif (user == "Y"):
+        ClientSocket.send("Enter your Ashoka email:".encode())
+        #wait for response
+        email = ClientSocket.recv(3000).decode() #gets email from client
+        if email.endswith('@ashoka.edu.in'):
+            ClientSocket.send("Enter your password:".encode())
+            #generate and send password to email
+            password = ClientSocket.recv(3000).decode()
+            #check if password is valid
+            if(users[email] == password):
+                ClientSocket.send("You have been logged in successfully.".encode())
+                if(ClientSocket.recv(3000).decode() != "OK"):
+                    print("Disconnecting! Error!!")
+                    ClientSocket.close()
+                handle_new_client(ClientSocket,Address)
+        else:
+            ClientSocket.send("Invalid email".encode())
+    else:
+        ClientSocket.send("Invalid choice! Disconnecting...".encode())
+
+    with open("users.json", "w") as jsonFile:
+        json.dump(users, jsonFile)
+
+    ClientSocket.close()
+
+print("Server is going down!")
+ServerSocket.close()
+
+
+#Sequence of events
+#Client connects to server
+#Server accepts connection
+
+#(Interaction begins)
+#Server sends intro message -> welcome to ams
+#Client sends OK response 
+#Server sends list of services
+#Client sends choice
+#If n, server sends outro message??
+#If prereqs, server sends prereqs request
+#Client sends sem/course code
+#Server sends OK response -- has to return data
+#If choice invalid, server sends error message
